103c7b107d29f14eb146f590c135cc6b765d1b51
Rostyslav Bysko <brn.h74@gmail.com>
2025-02-09 19:09:23 +0200

    edit

--- scripts/create_django_auto_admin.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-PUSHD "../project_1444"
-set PYTHONPATH=.
-poetry run python ./utils/create_groups.py
-poetry run python ./utils/create_admin_user.py
-POPD
--- scripts/gen_req_txt.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-PUSHD ".."
-@REM poetry env info -p
-@REM echo "\Scripts\activate"
-poetry export --without-hashes > requirements.txt
-POPD
