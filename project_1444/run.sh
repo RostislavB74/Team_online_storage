6534397dc6c43c40c622f7c3566ce667c752f6b3
lexxai <lexxai@gmail.com>
2025-02-25 00:03:56 +0200

    run docker

--- project_1444/run.sh
+++ project_1444/run.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-gunicorn --bind "0.0.0.0:8000" "project_1444.wsgi:application"
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+# Ensure DEBUG is set
+if [ "$DEBUG" = "False" ]; then
+   python manage.py runserver 0.0.0.0:8000 --noreload --insecure --no-color
+  # Run Gunicorn in production
+#  gunicorn --bind "0.0.0.0:8000" "project_1444.wsgi:application"
+else
+  # Run Django development server
+  python manage.py runserver 0.0.0.0:8000 --noreload --insecure --no-color
+fi
+#gunicorn --bind "0.0.0.0:8000" "project_1444.wsgi:application"
