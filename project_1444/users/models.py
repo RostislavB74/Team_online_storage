ef191374dc1334fdb297faf13b619d1bce32fc97
Rostyslav Bysko <brn.h74@gmail.com>
2025-03-08 21:57:05 +0200

    add category serializer

--- project_1444/users/models.py
+++ project_1444/users/models.py
@@ -5,17 +5,9 @@ from django.contrib.auth.models import Group
 from django.db import transaction
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from rest_framework_simplejwt.tokens import RefreshToken
-
-
-# User Manager
-import phonenumbers
-from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group
-from django.db import models
-from ipware import get_client_ip
 
 class UserManager(BaseUserManager):
     def get_country_from_ip(self, request):
@@ -81,49 +73,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         return self.create_user(name=name, email=email, password=password, **extra_fields)
 
-# class UserManager(BaseUserManager):
-#     def normalize_phone(self, phone, country="UA"):
-#         """Перевіряє, нормалізує та повертає телефон у міжнародному форматі."""
-#         if not phone:
-#             return None  
 
-#         try:
-#             parsed_number = phonenumbers.parse(phone, country)
-#             if not phonenumbers.is_valid_number(parsed_number):
-#                 raise ValidationError("Invalid phone number")
-#             return phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)  # +380XXXXXXXXX
-#         except phonenumbers.NumberParseException:
-#             raise ValidationError("Invalid phone number format")
-
-#     def create_user(self, name, email=None, phone=None, telegram=None, password=None, **extra_fields):
-#         if not name:
-#             raise ValueError("User must have a name")
-
-#         if not email and not phone and not telegram:
-#             raise ValueError("User must have at least one contact method (email, phone, or Telegram)")
-
-#         email = self.normalize_email(email) if email else None
-#         phone = self.normalize_phone(phone) 
-
-#         with transaction.atomic():  
-#             user = self.model(name=name, email=email, phone=phone, telegram=telegram, **extra_fields)
-
-#             if password:
-#                 user.set_password(password)
-#             else:
-#                 user.set_unusable_password()
-
-#             user.save(using=self._db)
-
-#             group, _ = Group.objects.get_or_create(name="Користувачі")
-#             user.groups.add(group)
-
-#         return user
-
-#     def create_superuser(self, name, email, password, **extra_fields):
-#         extra_fields.setdefault("is_staff", True)
-#         extra_fields.setdefault("is_superuser", True)
-#         return self.create_user(name=name, email=email, password=password, **extra_fields)
 
 phone_validator = RegexValidator(
     regex=r"^\+\d{1,15}$",
