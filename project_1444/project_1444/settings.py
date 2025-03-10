7312cd097b260e7d5f0ec25b62a2e271df4934e1
lexxai <lexxai@gmail.com>
2025-03-10 22:24:39 +0200

    STATIC_URL ENV

--- project_1444/project_1444/settings.py
+++ project_1444/project_1444/settings.py
@@ -196,7 +196,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = env('STATIC_URL', default='/static/')  # 'static/'
+
 # for manage.py collect static
 STATIC_ROOT = BASE_DIR / "static"
 # for additional static files outside static_root
