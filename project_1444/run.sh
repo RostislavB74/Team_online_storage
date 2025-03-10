d0feb963abd6467bdcf6c904c2d6468bf8a5c742
lexxai <lexxai@gmail.com>
2025-03-11 00:07:22 +0200

    gunicorn --bind "0.0.0.0:8000" "project_1444.wsgi:application"

--- project_1444/run.sh
+++ project_1444/run.sh
@@ -17,12 +17,13 @@ python manage.py collectstatic --noinput
 # Ensure DEBUG is set
 DEBUG=${DEBUG:-False}
 
-if [ "$DEBUG" = "False" ]; then
-   python manage.py runserver 0.0.0.0:8000 --noreload --insecure --no-color
-  # Run Gunicorn in production
+#if [ "$DEBUG" = "False" ]; then
+#  # python manage.py runserver 0.0.0.0:8000 --noreload --insecure --no-color
+#  # Run Gunicorn in production
 #  gunicorn --bind "0.0.0.0:8000" "project_1444.wsgi:application"
-else
-  # Run Django development server
-  python manage.py runserver 0.0.0.0:8000 --noreload --insecure --no-color
-fi
-#gunicorn --bind "0.0.0.0:8000" "project_1444.wsgi:application"
+#else
+#  # Run Django development server
+#  python manage.py runserver 0.0.0.0:8000 --noreload --insecure --no-color
+#fi
+
+gunicorn --bind "0.0.0.0:8000" "project_1444.wsgi:application"
