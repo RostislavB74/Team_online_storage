926f0a96aebc55e72cb0692cda9c4678adf2c280
Rostyslav Bysko <brn.h74@gmail.com>
2025-03-11 07:35:13 +0200

    add CategoriesViewset

--- project_1444/product/urls.py
+++ project_1444/product/urls.py
@@ -11,11 +11,11 @@
 # ]
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, RingSizeLookup
+from .views import ProductViewSet, RingSizeLookup, CategoriesViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='products')
-
+router.register(r'categories', CategoriesViewSet, basename='categories')
 urlpatterns = [
     path('', include(router.urls)),
     path('ring-size/', RingSizeLookup.as_view(), name='ring-size-lookup'),  # Додаємо окремо
