ebf137c57db07e51389cda59276f49425aa67379
lexxai <lexxai@gmail.com>
2025-03-13 04:18:37 +0200

    MixinCacheHeaders

---
+++ project_1444/utils/cache_headers.py
@@ -0,0 +1,35 @@
+import hashlib
+
+from django.core.exceptions import FieldError
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.utils.http import http_date
+from django.views.decorators.http import condition
+from rest_framework.response import Response
+
+
+class MixinCacheHeaders:
+
+    def get_cache_data(self):
+        """Отримати останній оновлений об'єкт і створити кеш-значення"""
+        try:
+            latest_object = self.get_queryset().order_by("-updated_at").first()
+        except FieldError:
+            latest_object = None
+
+        if latest_object:
+            last_modified = latest_object.updated_at
+            etag = hashlib.md5(str(last_modified).encode()).hexdigest()
+        else:
+            last_modified = timezone.now()
+            etag = None  # або можна задати дефолтне значення
+
+        return last_modified, etag
+
+    def add_cache_headers(self, response: Response):
+        """Attach cache headers to the response"""
+        last_modified, etag = self.get_cache_data()
+        response["Last-Modified"] = http_date(last_modified.timestamp())
+        if etag:
+            response["ETag"] = etag
+        return response
