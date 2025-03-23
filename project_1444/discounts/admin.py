617f977707e48cc544ca0753d644fe2ffbcd38b9
Rostyslav Bysko <brn.h74@gmail.com>
2025-03-23 18:49:02 +0200

    add fields in promocode

--- project_1444/discounts/admin.py
+++ project_1444/discounts/admin.py
@@ -6,6 +6,7 @@ class PromoCodeAdmin(admin.ModelAdmin):
     list_display = ("code", "discount_percentage", "valid_from", "valid_to", "is_active", "usage_limit", "used_count")
     search_fields = ("code",)
     list_filter = ("is_active", "valid_from", "valid_to")
+    filter_horizontal=("applicable_products", "applicable_categories")
     verbose_name = "Промокоди"
     verbose_name_plural = "Промокоди"
 
@@ -14,5 +15,6 @@ class CouponAdmin(admin.ModelAdmin):
     list_display = ("user", "discount_percentage", "valid_from", "valid_to", "is_active")
     list_filter = ("is_active", "valid_from", "valid_to")
     search_fields = ("user__email",)
+    
     verbose_name = "Купони на знижку"
     verbose_name_plural = "Купони на знижку"
