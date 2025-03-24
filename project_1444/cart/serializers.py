2d324d53c3298ececca959b3202e769b5824a879
Rostyslav Bysko <brn.h74@gmail.com>
2025-03-24 06:48:41 +0200

    edit cart serializer

--- project_1444/cart/serializers.py
+++ project_1444/cart/serializers.py
@@ -8,16 +8,25 @@ from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 from product.models import SubProducts
 from .models import Cart
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 class CartSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source="product.parent_product", read_only=True)
     total_price = serializers.SerializerMethodField()  # Обчислена ціна окремої позиції
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
+    username = serializers.CharField(source="user.username", read_only=True) 
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            cart = Cart.objects.create(**validated_data)
+            return cart
 
     class Meta:
         model = Cart
-        fields = ["id", "user", "product", "product_name", "quantity", "total_price", "created_timestamp"]
+        fields = ["id", "user", "username", "product", "product_name", "quantity", "total_price", "created_timestamp"]
 
     def get_total_price(self, obj):
         """Обчислює загальну ціну для одного товару в корзині"""
         return obj.products_price()
 
+    
