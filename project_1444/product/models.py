c1a9f6fdbdb0b29c31bd46556413c0006b5dbde9
Xuno <dev@xuno.com>
2025-04-08 02:09:53 +0300

    ProductCertificate file = MultiBackendImageField

--- project_1444/product/models.py
+++ project_1444/product/models.py
@@ -454,7 +454,8 @@ class ProductImage(models.Model):
 
 class ProductCertificate(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='certificates')
-    file = CloudinaryField("file")  # Змінюємо на CloudinaryField
+    file = MultiBackendImageField(upload_to="file/", blank=True, null=True)
+    # file = CloudinaryField("file")  # Змінюємо на CloudinaryField
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     class Meta: 
