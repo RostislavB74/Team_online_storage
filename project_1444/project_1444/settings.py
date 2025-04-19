fe73acc140e8e4e18a540b42d6a4289a3466329e
Rostyslav Bysko <brn.h74@gmail.com>
2025-04-19 09:58:16 +0300

    liqpay

--- project_1444/project_1444/settings.py
+++ project_1444/project_1444/settings.py
@@ -97,7 +97,7 @@ INSTALLED_APPS = [
     # "django_prices_vatlayer",
     # "sorl.thumbnail",
     # 
-    "django-liqpay",
+    # "django-liqpay",
     "product",
     'users.apps.UsersConfig',
     # "users",
