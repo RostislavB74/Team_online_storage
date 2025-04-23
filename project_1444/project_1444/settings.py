a8b87db58378f4697c3a61f546847e5c387c151c
LeXXai <lexxai@gmail.com>
2025-04-23 12:24:23 +0300

    dedupe "django.middleware.locale.LocaleMiddleware"

--- project_1444/project_1444/settings.py
+++ project_1444/project_1444/settings.py
@@ -153,7 +153,6 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
