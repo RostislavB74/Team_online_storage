525b384067879ec2b5f15370e64d87bb9a8286f5
LeXXai <lexxai@gmail.com>
2025-04-24 17:25:53 +0300

    refactoring black

--- project_1444/product/serializers.py
+++ project_1444/product/serializers.py
@@ -227,24 +227,26 @@ class SubProductsSizesSerializer(serializers.ModelSerializer):
 #         model = Categories
 #         fields = ['id', 'name', 'slug']
 
+
 class SubProductSerializer(serializers.ModelSerializer):
     size = serializers.SerializerMethodField()
 
     class Meta:
         model = SubProducts
-        fields = ['id', 'parent_product', 'price', 'sku', 'size']
+        fields = ["id", "parent_product", "price", "sku", "size"]
 
     @extend_schema_field(str)
     def get_size(self, obj):
         if obj.size:
-            if isinstance(obj.size, dict) and 'value' in obj.size:
-                return obj.size['value']
+            if isinstance(obj.size, dict) and "value" in obj.size:
+                return obj.size["value"]
             return str(obj.size)
         if obj.length and obj.max_length:
             return f"{obj.length}-{obj.max_length}"
         if obj.length:
             return str(obj.length)
         return ""
+
 
 # class ProductSerializer(serializers.ModelSerializer):
 #     category = CategorySerializer()
@@ -258,35 +260,36 @@ from rest_framework import serializers
 from drf_spectacular.utils import extend_schema_field
 from .models import Product, SubProducts, Categories, SubCategories
 
+
 class CategorySerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField()
     slug = serializers.SerializerMethodField()
-    
 
     class Meta:
         model = Categories
-        fields = ['id', 'name', 'slug']
+        fields = ["id", "name", "slug"]
 
     @extend_schema_field(str)
     def get_name(self, obj):
-        return obj.safe_translation_getter('name', default='Без назви')
+        return obj.safe_translation_getter("name", default="Без назви")
 
     @extend_schema_field(str)
     def get_slug(self, obj):
-        return obj.safe_translation_getter('slug', default=None)
+        return obj.safe_translation_getter("slug", default=None)
+
 
 class SubProductSerializer(serializers.ModelSerializer):
     size = serializers.SerializerMethodField()
 
     class Meta:
         model = SubProducts
-        fields = ['id', 'parent_product', 'price', 'sku', 'size']
+        fields = ["id", "parent_product", "price", "sku", "size"]
 
     @extend_schema_field(str)
     def get_size(self, obj):
         if obj.size:
-            if isinstance(obj.size, dict) and 'value' in obj.size:
-                return obj.size['value']
+            if isinstance(obj.size, dict) and "value" in obj.size:
+                return obj.size["value"]
             return str(obj.size)
         if obj.length and obj.max_length:
             return f"{obj.length}-{obj.max_length}"
@@ -294,13 +297,15 @@ class SubProductSerializer(serializers.ModelSerializer):
             return str(obj.length)
         return ""
 
+
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
-    subproducts = SubProductSerializer(many=True, source='subproducts_set')
+    subproducts = SubProductSerializer(many=True, source="subproducts_set")
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'category', 'sku', 'subproducts']
+        fields = ["id", "name", "category", "sku", "subproducts"]
+
 
 class SubCategoriesSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField()
@@ -309,15 +314,16 @@ class SubCategoriesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SubCategories
-        fields = ['id', 'name', 'slug', 'parent']
+        fields = ["id", "name", "slug", "parent"]
 
     @extend_schema_field(str)
     def get_name(self, obj):
-        return obj.safe_translation_getter('name', default='Без назви')
+        return obj.safe_translation_getter("name", default="Без назви")
 
     @extend_schema_field(str)
     def get_slug(self, obj):
-        return obj.safe_translation_getter('slug', default=None)
+        return obj.safe_translation_getter("slug", default=None)
+
 
 class ProductImageSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
