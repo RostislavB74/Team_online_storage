d0ca47a32533eca73e8983d312dc1b55b7b9692e
LeXXai <lexxai@gmail.com>
2025-05-03 01:41:13 +0300

    LOGIN_URL = "auth/login/" LOGIN_REDIRECT_URL = "user/profile/"

--- project_1444/project_1444/settings.py
+++ project_1444/project_1444/settings.py
@@ -23,8 +23,6 @@ from pathlib import Path
 import environ
 from datetime import timedelta
 
-from django.urls import reverse
-
 # from pygments.lexer import default
 # import os
 # from urllib.parse import urlparse
@@ -197,8 +195,8 @@ WSGI_APPLICATION = "project_1444.wsgi.application"
 AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.ModelBackend"]
 
 # URL для перенаправлення після логіну/логоуту
-LOGIN_URL = reverse("api_login")
-LOGIN_REDIRECT_URL = reverse("api_profile")
+LOGIN_URL = "auth/login/"
+LOGIN_REDIRECT_URL = "user/profile/"
 LOGOUT_REDIRECT_URL = "/admin/login/"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # Database
