355ee5e7bb288fc8ed991c41cf48bf79e62e57d8
LeXXai <lexxai@gmail.com>
2025-05-02 17:58:54 +0300

    test of users only for now

--- scripts/run_tests.cmd
+++ scripts/run_tests.cmd
@@ -1,7 +1,7 @@
 @echo off
 echo ""
-echo  "Starting Django test..."
+echo "Starting Django test..."
 PUSHD "../project_1444"
-SET apps="product"
+SET apps="users"
 poetry run python manage.py test %apps% --keepdb -v 2
 POPD
