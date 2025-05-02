5738502315ab72ab836ecb7048b56d0e6e95e4df
LeXXai <lexxai@gmail.com>
2025-05-02 18:04:12 +0300

    test_login_user

---
+++ project_1444/users/tests/tests_auth_api.py
@@ -0,0 +1,67 @@
+import random
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.crypto import get_random_string
+from rest_framework.test import APIClient
+
+
+User = get_user_model()
+
+
+class AuthAPITest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()  # Don't forget this!
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
+        cls.user_test = {
+            "username": "test_user",
+            "password": get_random_string(10),
+            "email": f"test_user{random.randint(1, 100)}@test.com",
+        }
+        print("Generated test user:", cls.user_test)
+        cls.crud()
+
+    @classmethod
+    def create_superuser(cls):
+        cls.user = User.objects.create_superuser(username="admin", password="admin")
+        print("Created superuser:", cls.user)
+
+    @classmethod
+    def crud(cls):
+        # print("CRUD")
+        cls.create_superuser()
+
+    def login(self, user: User):
+        self.client.force_login(user=user)
+
+    def logout(self):
+        self.client.logout()
+
+    def create_user_unit(
+        self, username: str, password: str | None = None, email: str = None
+    ):
+        assert username, "Username is required"
+        user = User.objects.create_user(
+            username=username, email=email, password=password
+        )
+        print("Created user:", user)
+        return user
+
+    def setUp(self):
+        # Logout before each test
+        self.logout()
+
+    def test_login_user(self):
+        user = self.create_user_unit(**self.user_test)
+        assert user, "Test user is not created"
+        data = {
+            "username": self.user_test["username"],
+            "password": self.user_test["password"],
+        }
+        response = self.client.post(reverse("api_login"), data, format="json")
+        self.assertEqual(response.status_code, 200)
