5b15399cada1f681487522ea10eb60fdc9cb45d9
LeXXai <lexxai@gmail.com>
2025-05-03 14:19:09 +0300

    return "api/v1/auth/" for api doc logins

--- project_1444/project_1444/urls.py
+++ project_1444/project_1444/urls.py
@@ -64,7 +64,7 @@ urlpatterns = [
     path("api/v1/", include("order.urls")),
     path("api/v1/", include("cart.urls")),
     path("api/v1/", include("product.urls")),
-    # path("api/v1/auth/", include("rest_framework.urls")),
+    path("api/v1/auth/", include("rest_framework.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/schema/swagger-ui/",
