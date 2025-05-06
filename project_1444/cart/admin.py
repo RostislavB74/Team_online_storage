48a29b78c52680da3b71f8813f21675ca24de549
LeXXai <lexxai@gmail.com>
2025-05-07 00:13:08 +0300

    translate carts

--- project_1444/cart/admin.py
+++ project_1444/cart/admin.py
@@ -1,48 +1,59 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
 from .models import Cart
+
 # from product.models import SubProducts
+
 
 class CartTabAdmin(admin.TabularInline):
     model = Cart
-    fields = ("product", "quantity", "product_price", "product_sum","created_timestamp") 
-    search_fields = ("product__name", "quantity", "created_timestamp") 
-    readonly_fields = ("created_timestamp", "product_price")  
+    fields = (
+        "product",
+        "quantity",
+        "product_price",
+        "product_sum",
+        "created_timestamp",
+    )
+    search_fields = ("product__name", "quantity", "created_timestamp")
+    readonly_fields = ("created_timestamp", "product_price")
     extra = 1
     fieldsets = (
-        ("Товари в корзині", {
-            "fields": (
-                "product", "product_price", "quantity", "created_timestamp" 
-            ),
-        }),
-        ("Загальна вартість корзини", {
-            "fields": (
-                "product_sum",
-            )
-        })
+        (
+            _("Товари в корзині"),
+            {
+                "fields": ("product", "product_price", "quantity", "created_timestamp"),
+            },
+        ),
+        (_("Загальна вартість корзини"), {"fields": ("product_sum",)}),
     )
-   
+
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ["user_display", "product_display",  "product_price", "quantity", "product_sum", "created_timestamp"]
+    list_display = [
+        "user_display",
+        "product_display",
+        "product_price",
+        "quantity",
+        "product_sum",
+        "created_timestamp",
+    ]
     list_filter = ["created_timestamp", "user"]
-    
-    
 
     def user_display(self, obj):
-        return str(obj.user) if obj.user else "Анонімний користувач"
+        return str(obj.user) if obj.user else _("Анонімний користувач")
 
     def product_display(self, obj):
         return str(obj.product)
 
     def product_price(self, obj):
         return obj.product.price
+
     def product_sum(self, obj):
         return obj.product.price * obj.quantity
-    
-    user_display.short_description = "Користувач"
-    product_display.short_description = "Товар"
-    product_price.short_description = "Ціна товару"
-    product_sum.short_description = "Сума"
 
-
+    user_display.short_description = _("Користувач")
+    product_display.short_description = _("Товар")
+    product_price.short_description = _("Ціна товару")
+    product_sum.short_description = _("Сума")
