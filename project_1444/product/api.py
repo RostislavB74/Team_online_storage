73de0fd21b179d90d35ee23118ecfde70ee0ffc7
LeXXai <lexxai@gmail.com>
2025-09-04 17:20:19 +0300

    test gen openapi

--- project_1444/product/api.py
+++ project_1444/product/api.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
-from .models import Product, ProductImage, ProductCertificate
-from .serializers import ProductSerializer, ProductImageSerializer, ProductCertificateSerializer, RingSizeSerializer
+from .models import Product
+from .serializers import (
+    RingSizeSerializer,
+)
 
 # class ProductViewSet(viewsets.ModelViewSet):
 #     queryset = Product.objects.all()
@@ -13,11 +15,13 @@ from .serializers import ProductSerializer, ProductImageSerializer, ProductCerti
 # class ProductCertificateViewSet(viewsets.ModelViewSet):
 #     queryset = ProductCertificate.objects.all()
 #     serializer_class = ProductCertificateSerializer
+
+
 class RingSizeViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = RingSizeSerializer
 
     def get_serializer_class(self):
-        if self.action == 'list':
+        if self.action == "list":
             return RingSizeSerializer
         return super().get_serializer_class()
