21b93f16b11f7be6f55ee410581b0745e1b72c7a
LeXXai <lexxai@gmail.com>
2025-09-07 19:21:39 +0300

    Refactoring: generate_material_article, product_pre_save, generate_product_new_article

--- project_1444/product/signals.py
+++ project_1444/product/signals.py
@@ -1,9 +1,16 @@
 from django.core.cache import cache
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
-from django_redis import get_redis_connection
 
-from .models import Product, ProductAttributes, SubProducts, Categories, SubCategories
+from .models import (
+    Product,
+    ProductAttributes,
+    Categories,
+    SubCategories,
+    Material,
+    generate_sku,
+    generate_product_new_article,
+)
 
 
 @receiver(post_save, sender=Product)
@@ -38,3 +45,32 @@ def category_changed(sender, instance, **kwargs):
 @receiver(post_delete, sender=SubCategories)
 def subcategory_changed(sender, instance, **kwargs):
     clear_category_tree_cache()
+
+
+# Функція для генерації артикула перед збереженням
+@receiver(pre_save, sender=Material)
+def generate_material_article(sender, instance, **kwargs):
+    if not instance.article:
+        metal_code = (
+            instance.material[0].upper() if instance.material else ""
+        )  # Перевірка, чи є metal
+        color_code = (
+            instance.color[0].upper() if instance.color else ""
+        )  # Перевірка, чи є color
+        assay_code = (
+            str(instance.assay) if instance.assay else ""
+        )  # Перевірка, чи є assay
+        last_material = Material.objects.order_by("-id").first()
+        next_number = (
+            f"{(last_material.id + 1) if last_material else 1:03d}"  # Генерація номера
+        )
+        instance.article = f"{metal_code}{assay_code}{color_code}{next_number}"
+
+
+# Сигнал `pre_save` для автоматичного заповнення SKU, артикулу та QR-коду
+@receiver(pre_save, sender=Product)
+def product_pre_save(sender, instance, **kwargs):
+    if not instance.sku:
+        instance.sku = generate_sku()
+    if not instance.article:
+        instance.article = generate_product_new_article()
