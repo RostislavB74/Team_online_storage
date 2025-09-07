d0a6439502872087c46791cdfc115a7bcc5c4734
LeXXai <lexxai@gmail.com>
2025-09-07 18:31:41 +0300

    request: Request | HttpRequest

--- project_1444/utils/language_code.py
+++ project_1444/utils/language_code.py
@@ -1,9 +1,12 @@
 from django.conf import settings
+from django.http import HttpRequest
 from django.utils.translation import get_language
 from rest_framework.request import Request
 
 
-def get_language_code(request: Request, default: str = settings.LANGUAGE_CODE) -> str:
+def get_language_code(
+    request: Request | HttpRequest, default: str = settings.LANGUAGE_CODE
+) -> str:
     lang = request.GET.get("lang", "").strip()[:6].lower()
     if lang == "" and ("Accept-Language" in request.headers):
         # lang = request.headers["Accept-Language"].split(",")[0][:6].lower()
