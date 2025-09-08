204f29a00d34df3f13c6c297c4b9bf70d5ccc8b7
LeXXai <lexxai@gmail.com>
2025-09-08 03:53:05 +0300

    fix compatibility with python 3.11 f"{self.pk:03d}-{field_translated}"

--- project_1444/product/models.py
+++ project_1444/product/models.py
@@ -36,7 +36,8 @@ class Categories(TranslatableModel):
         verbose_name_plural = _("Категорії виробів")
 
     def __str__(self):
-        return f"{self.pk:03d}-{self.safe_translation_getter("name", default=_("Без назви"))}"
+        field_translated = self.safe_translation_getter("name", default=_("Без назви"))
+        return f"{self.pk:03d}-{field_translated}"
 
 
 class Weaving(TranslatableModel):
@@ -114,7 +115,8 @@ class SubCategories(TranslatableModel):
         verbose_name_plural = _("Підкатегорії виробів")
 
     def __str__(self):
-        return f"{self.pk:03d}-{self.safe_translation_getter("name", default=_("Без назви"))}"
+        field_translated = self.safe_translation_getter("name", default=_("Без назви"))
+        return f"{self.pk:03d}-{field_translated}"
 
 
 class Colors(TranslatableModel):
@@ -150,7 +152,8 @@ class Collections(TranslatableModel):
         verbose_name_plural = _("Колекції")
 
     def __str__(self):
-        return f"{self.pk:03d}-{self.safe_translation_getter("name", default=_("Без назви"))}"
+        field_translated = self.safe_translation_getter("name", default=_("Без назви"))
+        return f"{self.pk:03d}-{field_translated}"
 
 
 class Designs(TranslatableModel):
