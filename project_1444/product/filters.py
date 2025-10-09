19e55e277eb73dfc814f087e9e3561a0e701ad81
Rostyslav Bysko <brn.h74@gmail.com>
2025-10-09 09:40:33 +0300

    add lenght

--- project_1444/product/filters.py
+++ project_1444/product/filters.py
@@ -113,6 +113,9 @@ class ProductFilter(filters.FilterSet):
         # Фільтруємо продукти, де є subproduct з size, близьким до заданого (наприклад, ±0.5)
         return queryset.filter(
             Q(subproducts__size__gte=size_float - 0.5) & Q(subproducts__size__lte=size_float + 0.5)
+            or Q(subproducts__length__gte=size_float - 0.5) & Q(subproducts__length__lte=size_float + 0.5)
+            or Q(subproducts__max_length__gte=size_float - 0.5) & Q(subproducts__max_length__lte=size_float + 0.5)
+            or Q(subproducts__width__gte=size_float - 0.5) & Q(subproducts__width__lte=size_float + 0.5)
         ).distinct()
 
 
